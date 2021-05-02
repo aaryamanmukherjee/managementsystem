34c776d9303a53863e65735f3df5810c98a4289c
Aaryaman Mukherjee <74101332+aaryamanmukherjee@users.noreply.github.com>
2021-05-02 12:35:25 +0530

    Add files via upload

---
+++ library management system IP.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  1 23:18:09 2021
+
+@author: aarya
+"""
+
+import mysql.connector as a
+con = a.connect (host="localhost",user="root",passwd="1234",database="library")
+
+
+def addbook():
+    bn = input('Enter BOOK Name :')
+    c = input('Enter BOOK Code: ')
+    t = input('Total Books: ')
+    s = input('Enter Subject : ')
+    data = (bn,c,t,s)
+    sql = 'insert into books values(%s,%s,%s,%s)'
+    c = con.cursor()
+    c.execute(sql,data)
+    con.commit()
+    print(">______________________________________________________________<")
+    print('Data Entered Successfully')
+    main()
+
+def issueb():
+    n = input('Enter Name :')
+    r = input('Enter Reg no :')
+    co = input('Enter Book Code :')
+    d = input('Enter Date :')
+    a = "insert into issue values(%s,%s,%s,%s)"
+    data =(n,r,co,d)
+    c = con.cursor()
+    c.execute(a,data)
+    con.commit()
+    print('>__________________________________________________________________<')
+    print("Book Issued to :", n)
+    bookup(co,-1)
+
+def submitb():
+    n = input('Enter Name :')
+    r = input('Enter Reg no :')
+    co = input('Enter Book Code :')
+    d = input('Enter Date :')
+    a = "insert into submit values(%s,%s,%s,%s)"
+    data =(n,r,co,d)
+    c = con.cursor()
+    c.execute(a,data)
+    con.commit()
+    print('>__________________________________________________________________<')
+    print("Book submitted from :", n)
+    bookup(co,1)
+
+def bookup(co,u):
+    a="select TOTAL from books where BCODE = %s"
+    data = (co,)
+    c = con.cursor()
+    c.execute(a,data)
+    myresult = c.fetchone()
+    t = myresult[0] + u
+    sql = "update books set TOTAL = %s where BCODE = %s"
+    d = (t,co)
+    c.execute(sql,d)
+    c.execute(sql,d)
+    con.commit()
+    main()
+
+def dbook():
+    ac = input('Enter Book Code:')
+    a ="delete from books where BCODE = %s"
+    data = (ac,)
+    c = con.cursor()
+    c.execute(a,data)
+    con.commit()
+    main()
+
+def dispbook():
+    a ="select*from books"
+    c = con.cursor()
+    c.execute(a)
+    myresult = c.fetchall()
+    for i in myresult:
+        print('BOOK NAME : ',i[0])
+        print('BOOK CODE : ',i[1])
+        print('TOTAL : ',i[2])
+        print('_____________________________')
+    main()
+
+def main():
+    print("""                                         
+                
+    
+                    LIBRARY MANAGER
+    1. ADD BOOK
+    2. ISSUE BOOK
+    3. SUBMIT BOOK 
+    4. DELETE BOOK 
+    5. DISPLAY BOOKS
+
+
+    """)
+
+    choice = input('ENTER TASK NO: ')
+    print(">_____________________________________________________________<")
+    if(choice == '1'):
+        addbook()
+    elif(choice=='2'):
+        issueb()
+    elif(choice=='3'):
+        submitb()
+    elif(choice=='4'):
+        dbook()
+    elif(choice=='5'):
+        dispbook()
+    else:
+        print('Invalid choice........')
+        main()
+
+def pswd():
+    ps = input("Enter Password :")
+    if ps == '1234':
+        main()
+    else:
+        print('Wrong Password')
+        pswd()
+pswd()
